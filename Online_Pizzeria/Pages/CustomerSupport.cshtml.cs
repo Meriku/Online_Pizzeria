e3d0db20349034366e2f57b2bdd2aca788fabfc3
Meriku <m.mirik.v@gmail.com>
2022-11-06 00:00:44 +0300

    Added Customer Support page

---
+++ Online_Pizzeria/Pages/CustomerSupport.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Online_Pizzeria.Pages
+{
+    public class CustomerSupport : PageModel
+    {
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        private readonly IConfiguration _configuration;
+        public CustomerSupport(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            PhoneNumber = _configuration.GetValue<string>("Company:PhoneNumber") ?? "Change in AppSettings";
+            Address = _configuration.GetValue<string>("Company:Address") ?? "Change in AppSettings";
+            Email = _configuration.GetValue<string>("Company:Email") ?? "deafult@company.com";
+        }
+    }
+}
