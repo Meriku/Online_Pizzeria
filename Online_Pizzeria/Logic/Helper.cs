8ff35fb6e222bb3d091d3eb004a600056ad47938
Meriku <m.mirik.v@gmail.com>
2022-11-12 16:08:58 +0100

    Updated to use with AWS DataBase

--- Online_Pizzeria/Logic/Helper.cs
+++ Online_Pizzeria/Logic/Helper.cs
@@ -61,5 +61,17 @@ namespace Online_Pizzeria.Logic
             }
         }
 
+        public static string GetConnectionString(this WebApplicationBuilder builder)
+        {
+            var appConfig = builder.Configuration;
+
+            string dbname = appConfig.GetSection("RDS_DB_NAME").Value;
+            string username = appConfig["RDS_USERNAME"];
+            string password = appConfig["RDS_PASSWORD"];
+            string hostname = appConfig["RDS_HOSTNAME"];
+            string port = appConfig["RDS_PORT"];
+
+            return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
+        }
     }
 }
--- Online_Pizzeria/Program.cs
+++ Online_Pizzeria/Program.cs
@@ -1,15 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Online_Pizzeria.DataBase;
+using Online_Pizzeria.Logic;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.GetConnectionString();
 
-builder.Services.AddDbContext<ApplicationDB>(options => options.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=PizzaApp;"));
+builder.Services.AddDbContext<ApplicationDB>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
 
 if (!app.Environment.IsDevelopment())
 {
